154539bc7e0d3a6505baec622d21bf92c5cbf69a
快乐浪子哥 <hulangfy@163.com>
2022-10-27 13:52:52 +0800

    update

---
+++ src/jump/command/SendConfig.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace think\jump\command;
+
+use think\console\Command;
+use think\console\Input;
+use think\console\input\Argument;
+use think\console\input\Option;
+use think\console\Output;
+use think\facade\Env;
+
+class SendConfig extends Command
+{
+
+    public function configure()
+    {
+        $this->setName('jump:config')
+            ->setDescription('send config to tp folder');
+    }
+
+    public function execute(Input $input, Output $output)
+    {
+        //获取默认配置文件
+        $content = file_get_contents(root_path() . 'vendor/hulang/think-jump/src/config.php');
+
+        $configPath = config_path() . '/';
+        $configFile = $configPath . 'jump.php';
+
+
+        //判断目录是否存在
+        if (!file_exists($configPath)) {
+            mkdir($configPath, 0755, true);
+        }
+
+        //判断文件是否存在
+        if (is_file($configFile)) {
+            throw new \InvalidArgumentException(sprintf('The config file "%s" already exists', $configFile));
+        }
+
+        if (false === file_put_contents($configFile, $content)) {
+            throw new \RuntimeException(sprintf('The config file "%s" could not be written to "%s"', $configFile, $configPath));
+        }
+
+        $output->writeln('create jump config ok');
+    }
+}
