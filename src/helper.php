c63bb2df6e7d90da6c587b5a90e9d335fc0751c3
快乐浪子哥 <hulangfy@163.com>
2024-04-23 16:16:15 +0800

    update

--- src/helper.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-\think\Console::starting(function (\think\Console $console) {
-    $console->addCommands([
-        'jump:config' => '\\think\\jump\\command\\SendConfig'
-    ]);
-});
